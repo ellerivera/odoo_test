84e561f332b14f0c85f1eed54220d46f9dc709fb
daniela.rivera <danielarivera@gmail.com>
2024-05-07 04:20:13 -0400

    First commit

---
+++ crm_customization/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "CRM Customization",
+    'summary': """ This is a module created for custom the CRM module in Odoo v16. """,
+    'author': "Daniela Rivera Feria",
+    'website': " ",
+    'application': True,
+    'category': 'sales',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['crm'],
+
+    # always loaded
+    'data': [],
+
+    # only loaded in demonstration mode
+    'demo': [],
+    'installable': True,
+    'auto_install': False
+}
