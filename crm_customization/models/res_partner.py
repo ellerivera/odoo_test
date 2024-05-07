e7d08f743932f4d89d7c926f1f5a59162e7ec9f3
daniela.rivera <danielarivera@gmail.com>
2024-05-07 19:13:11 -0400

    Modify social networks view second task

--- crm_customization/models/res_partner.py
+++ crm_customization/models/res_partner.py
@@ -7,3 +7,11 @@ class Partner(models.Model):
     url_facebook = fields.Char('Facebook URL', help='URL of your profile on the social network', copy=False)
     url_twitter = fields.Char('Twitter URL', help='URL of your profile on the social network', copy=False)
     url_linkedin = fields.Char('Linkedin URL', help='URL of your profile on the social network', copy=False)
+
+    complete_profile = fields.Boolean('Complete Profile', compute='_compute_complete_profile', default=False)
+
+    @api.depends('url_facebook', 'url_twitter', 'url_linkedin')
+    def _compute_complete_profile(self):
+        for record in self:
+            if record.url_facebook and record.url_twitter and record.url_linkedin:
+                record.complete_profile = True
