bc39bfcba666ed7a1eca1ef736ebc71fdc3eaaba
daniela.rivera <danielarivera@gmail.com>
2024-05-08 00:11:17 -0400

    Add filter incomplete profile fourth task

--- crm_customization/models/res_partner.py
+++ crm_customization/models/res_partner.py
@@ -9,7 +9,8 @@ class Partner(models.Model):
     url_twitter = fields.Char('Twitter URL', help='URL of your profile on the social network', copy=False)
     url_linkedin = fields.Char('Linkedin URL', help='URL of your profile on the social network', copy=False)
 
-    complete_profile = fields.Boolean('Complete Profile', compute='_compute_complete_profile', default=False)
+    complete_profile = fields.Boolean('Complete Profile', compute='_compute_complete_profile',
+                                      search='_search_complete_profile', default=False)
 
     @api.depends('url_facebook', 'url_twitter', 'url_linkedin')
     def _compute_complete_profile(self):
@@ -18,3 +19,14 @@ class Partner(models.Model):
                 record.complete_profile = True
             else:
                 record.complete_profile = False
+
+    def _search_complete_profile(self, operator, value):
+        if operator == '=':
+            records = self.search([('url_facebook', '!=', False), ('url_twitter', '!=', False),
+                                   ('url_linkedin', '!=', False)]) if value else self.search(
+                ['|', '|', ('url_facebook', '=', False), ('url_twitter', '=', False), ('url_linkedin', '=', False)])
+        else:
+            records = self.search(['|', '|', ('url_facebook', '=', False), ('url_twitter', '=', False),
+                                   ('url_linkedin', '=', False)]) if value else self.search(
+                [('url_facebook', '!=', False), ('url_twitter', '!=', False), ('url_linkedin', '!=', False)])
+        return [('id', 'in', records.ids)]
