be0eb13a30c924c71cd1ae786b33f07d8bd78e21
daniela.rivera <danielarivera@gmail.com>
2024-05-09 19:17:52 -0400

    Documenting code

--- crm_customization/models/res_partner.py
+++ crm_customization/models/res_partner.py
@@ -1,30 +1,36 @@
 from odoo import models, fields, api
 
 
+# This class inherits from res.partner, a model for partners/contacts in Odoo.
 class Partner(models.Model):
     _inherit = "res.partner"
 
+    # Social network URLs
+    # Three fields are defined to store the URLs of the partner’s Facebook, Twitter, and LinkedIn profiles.
     url_facebook = fields.Char('Facebook URL', help='URL of your profile on the social network', copy=False)
     url_twitter = fields.Char('Twitter URL', help='URL of your profile on the social network', copy=False)
     url_linkedin = fields.Char('Linkedin URL', help='URL of your profile on the social network', copy=False)
 
+    # Field to check if the profile is complete
+    # A Boolean field that is computed based on the presence of all three social network URLs.
     complete_profile = fields.Boolean('Complete Profile', compute='_compute_complete_profile',
                                       search='_search_complete_profile', default=False)
 
+    # Compute function to set 'complete_profile' to True if all URLs are present
     @api.depends('url_facebook', 'url_twitter', 'url_linkedin')
     def _compute_complete_profile(self):
         for record in self:
-            if record.url_facebook and record.url_twitter and record.url_linkedin:
-                record.complete_profile = True
-            else:
-                record.complete_profile = False
+            record.complete_profile = all([record.url_facebook, record.url_twitter, record.url_linkedin])
 
+    # Search function to filter records based on the 'complete_profile' status
     def _search_complete_profile(self, operator, value):
         if operator == '=':
+            # If searching for records with a complete profile
             records = self.search([('url_facebook', '!=', False), ('url_twitter', '!=', False),
                                    ('url_linkedin', '!=', False)]) if value else self.search(
                 ['|', '|', ('url_facebook', '=', False), ('url_twitter', '=', False), ('url_linkedin', '=', False)])
         else:
+            # If searching for records without a complete profile
             records = self.search(['|', '|', ('url_facebook', '=', False), ('url_twitter', '=', False),
                                    ('url_linkedin', '=', False)]) if value else self.search(
                 [('url_facebook', '!=', False), ('url_twitter', '!=', False), ('url_linkedin', '!=', False)])
